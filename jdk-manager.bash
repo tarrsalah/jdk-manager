c7e9b9ba62f15ff74e5a04f85e1564909b9525fa
tarrsalah <tarrsalah@gmail.com>
2013-05-18 01:18:52 +0100

    QAD jdk manager

---
+++ jdk-manager.bash
@@ -0,0 +1,96 @@
+#! /usr/bin/env bash
+
+#set -u
+#set -e
+
+JDK_ROOT="$HOME/local" # change it to the directory that contain the jdk's
+JDK_PATTERN="[0-9]\.[0-9]\.+([0-9])?(_+([0-9]))"
+CURRENT_JDK="$JDK_ROOT/.current_jdk"
+
+ 
+# colors 
+textred=$(tput setaf 1)
+textreset=$(tput sgr0)
+textgreen=$(tput setaf 2)
+
+########################################################################""
+# functions 
+########################################################################"
+
+# ascii art
+_print_java_ascii() {
+printf ${textred}"
+    _  ____  _  __  
+   / |/  _ \/ |/ /  
+   | || | \||   /   
+/\_| || |_/||   \   
+\____/\____/\_|\_\  
+                    
+" "\n"${textreset}                                                     
+}
+
+_is_jdk() {
+  return `[[ "$1" == jdk${JDK_PATTERN} ]]`
+}
+
+
+# init the current symlink
+_init_current() {
+  if [[ ! -L $CURRENT_JDK ]];
+  then
+    ln -s /dev/null $CURRENT_JDK
+  fi
+}
+
+_get_current_jdk() {
+  basename `readlink -f $CURRENT_JDK`
+}
+# list all available jdks in the JDK_ROOT directory
+_ls_jdks() {
+  for file in $(ls $JDK_ROOT)
+  do
+    if `_is_jdk "$file"`
+     then
+      if [[ $file == `_get_current_jdk` ]]
+       then
+	printf "${textgreen} > $file\n"
+      else
+	printf "${textreset}   $file\n"
+      fi
+    fi
+  done
+}
+
+_set_current_jdk() {
+  jdk="jdk$1"
+  if `_is_jdk $jdk` && [[ -d "$JDK_ROOT/$jdk" ]]
+  then
+    rm "$CURRENT_JDK"
+    ln -sf "$JDK_ROOT/$jdk" "$CURRENT_JDK"
+    printf "${textgreen} INFO: ${textreset}Updating the \$JAVA_HOME to $JDK_ROOT/${textgreen}`_get_current_jdk`${textreset}  .\n\n";
+    _ls_jdks;
+  else
+    printf "${textred} ERROR: ${textreset} there is no $jdk in $JDK_ROOT \n"
+  fi
+}
+
+########################################################################""
+# main
+########################################################################"
+
+
+
+export JAVA_HOME=$CURRENT_JDK;
+export PATH="$JAVA_HOME/bin:$PATH"
+
+jdk() {
+_print_java_ascii;
+_init_current;
+
+if [[ $# -eq 0 ]]
+then
+  _ls_jdks;
+else
+  _set_current_jdk $1
+fi
+}
