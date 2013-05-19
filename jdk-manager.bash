0b628368233a0c1002806d894791d363b31eaf57
tarrsalah <tarrsalah@gmail.com>
2013-05-20 00:58:32 +0100

    ascii-arts only in the first use of the session

--- jdk-manager.bash
+++ jdk-manager.bash
@@ -3,13 +3,16 @@
 #set -u
 #set -e
 
+#to modify
 JDK_ROOT="$HOME/local" # change it to the directory that contain the jdk's
+
 JDK_PATTERN="[0-9]\.[0-9]\.+([0-9])?(_+([0-9]))"
 CURRENT_JDK="$JDK_ROOT/.current_jdk"
+SESSION=0;
 
- 
 # colors 
 textred=$(tput setaf 1)
+texttyellow=$(tput setaf 3)
 textreset=$(tput sgr0)
 textgreen=$(tput setaf 2)
 
@@ -19,14 +22,15 @@ textgreen=$(tput setaf 2)
 
 # ascii art
 _print_java_ascii() {
-printf ${textred}"
-    _  ____  _  __  
-   / |/  _ \/ |/ /  
-   | || | \||   /   
-/\_| || |_/||   \   
-\____/\____/\_|\_\  
-                    
-" "\n"${textreset}                                                     
+printf ${texttyellow}"
+   _     _ _                                                     
+  (_) __| | | __     _ __ ___   __ _ _ __   __ _  __ _  ___ _ __ 
+  | |/ _  | |/ /____|  _   _ \ / _  |  _ \ / _  |/ _  |/ _ \  __|
+  | | (_| |   <_____| | | | | | (_| | | | | (_| | (_| |  __/ |   
+ _/ |\__ _|_|\_\    |_| |_| |_|\__ _|_| |_|\__ _|\__  |\___|_|   
+|__/                                             |___/           
+
+"${textreset}                                                     
 }
 
 _is_jdk() {
@@ -78,13 +82,15 @@ _set_current_jdk() {
 # main
 ########################################################################"
 
-
-
 export JAVA_HOME=$CURRENT_JDK;
 export PATH="$JAVA_HOME/bin:$PATH"
 
 jdk() {
-_print_java_ascii;
+ if [[ $SESSION -eq 0 ]]
+  then
+  _print_java_ascii;
+  SESSION=1;
+fi
 _init_current;
 
 if [[ $# -eq 0 ]]
